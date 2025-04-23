9aed41b43edfeb5ecc0080ace504200afd80c8de
Vinicius <viniciusgalmeida@gmail.com>
2025-04-23 10:41:02 -0300

    Add `Home.jsx` and `home.css`

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+/** @jsxImportSource preact */
+import { h } from "preact";
+// import Header from "../../components/header/Header";
+
+import "../../styles/global.css"; // Certifique-se que isso define estilos globais
+import "./home.css"; // Caso você queira estilos específicos para essa página
+
+const Home = () => {
+  return <></>;
+};
+
+export default Home;
