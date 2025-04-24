8f564aa8a50eefc266753ae59b887692cc4b20f2
Vinicius <viniciusgalmeida@gmail.com>
2025-04-24 20:47:34 -0300

    Fix layout structure in Home component by wrapping content in blueSquare div

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -10,16 +10,17 @@ import hospitalDesenhoEsquematico from "../../assets/hospitalDesenhoEsquematico.
 const Home = () => {
   return (
     <>
-      <div class={"blueSquare"} />
-      <h1>
-        LUCAS <br /> BATISTA
-      </h1>
-      <p class={"subtitle"}>Mestre em Arq. Hospitalar</p>
-      <img
-        src={hospitalDesenhoEsquematico}
-        class={"lucas_batista_logo"}
-        alt="Logo de Lucas Batista - Mestre em Arquitetura Hospitalar"
-      />
+      <div class={"blueSquare"}>
+        <h1>
+          LUCAS <br /> BATISTA
+        </h1>
+        <p class={"subtitle"}>Mestre em Arq. Hospitalar</p>
+        <img
+          src={hospitalDesenhoEsquematico}
+          class={"lucas_batista_logo"}
+          alt="Logo de Lucas Batista - Mestre em Arquitetura Hospitalar"
+        />
+      </div>
     </>
   );
 };
