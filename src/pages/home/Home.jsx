79843ac7a8987de4aeb7c55dcbf2ad33f0281f8b
Vinicius <viniciusgalmeida@gmail.com>
2025-04-24 20:44:56 -0300

    Update Home component layout and add logo image

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,14 +5,21 @@ import { h } from "preact";
 import "../../styles/global.css";
 import "./home.css";
 
+import hospitalDesenhoEsquematico from "../../assets/hospitalDesenhoEsquematico.png";
+
 const Home = () => {
   return (
     <>
-      <div class={"firstPart"} />
+      <div class={"blueSquare"} />
       <h1>
         LUCAS <br /> BATISTA
       </h1>
       <p class={"subtitle"}>Mestre em Arq. Hospitalar</p>
+      <img
+        src={hospitalDesenhoEsquematico}
+        class={"lucas_batista_logo"}
+        alt="Logo de Lucas Batista - Mestre em Arquitetura Hospitalar"
+      />
     </>
   );
 };
