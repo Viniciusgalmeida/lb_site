d782a70135cd8cb276b77ef761eb49f0b340b23f
Vinicius <viniciusgalmeida@gmail.com>
2025-04-25 16:45:21 -0300

    Add experiencia data and images for the Experiences section

---
+++ src/components/experiencias/experienciasData.js
@@ -0,0 +1,31 @@
+import hospital
+
+export const experiencias = [
+  {
+    id: 1,
+    title: "Arquiteto no Hospital Risoleta Tolentino Neves 2018 à 2025",
+    image: hospitalRisoleta,
+  },
+  {
+    id: 2,
+    title: "Master em Arquitetura Sanitária, Barcelona",
+    image: hospitalBarretos,
+  },
+  {
+    id: 3,
+    title: "Analista de Projetos Vigilância Sanitária de Ipatinga MG 2017",
+    image: hospitalClinicas,
+  },
+  {
+    id: 4,
+    title:
+      "Coordenação Geral de Urgência e Emergência. Ministério da Saúde 2012 à 2014",
+    image: hospitalRibeirao,
+  },
+  {
+    id: 5,
+    title:
+      "Especialização em arquitetura de hospitais, clínicas e laboratórios -INBEC",
+    image: hospitalRibeirao,
+  },
+];
