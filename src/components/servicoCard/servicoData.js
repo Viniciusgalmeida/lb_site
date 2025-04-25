ee8dc969cb36a0a6a97affbd1806dc7b6e37b5d9
Vinicius <viniciusgalmeida@gmail.com>
2025-04-25 14:33:05 -0300

    Add servicoData module with service information and images

---
+++ src/components/servicoCard/servicoData.js
@@ -0,0 +1,15 @@
+import medicalBuilding from "../../assets/medicalBuilding.svg";
+import architectBlueprint from "../../assets/architectBlueprint.svg";
+
+export const servcos = [
+  {
+    id: 1,
+    title: "Arquitetura Hospitalar",
+    image: medicalBuilding,
+  },
+  {
+    id: 2,
+    title: "Consultoria de Arquitetura",
+    image: architectBlueprint,
+  },
+];
