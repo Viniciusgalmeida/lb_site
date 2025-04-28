1ca93c5788b6701b0e82ce10a37ade45595ebb96
Vinicius <viniciusgalmeida@gmail.com>
2025-04-28 10:45:25 -0300

    Refactor LivroSection component with structured CSS for improved layout and styling

--- src/components/livro/LivroSection.jsx
+++ src/components/livro/LivroSection.jsx
@@ -1,13 +1,20 @@
+import "./livro.css";
+
 function LivroSection() {
   return (
-    <div>
-      <img src={"src/assets/hospitalArchitectureBook.png"} />
-      <p>
+    <div className="containerLivroSection">
+      <img
+        src={"src/assets/hospitalArchitectureBook.png"}
+        className="imageLivroSection"
+      />
+      <p className="textLivroSection">
         Trabalho final do mestrado desenvolvido em Barcelona, sob orientação do
         escritório de arquitetura Mario Corea, tese em forma de livro cujo tema
         é Hospital de Emergência para Região Metropolitana de Belo Horizonte.
       </p>
-      <button>Visualizar livro para donwload</button>
+      <button className="buttonLivroSection">
+        Visualizar livro para donwload
+      </button>
     </div>
   );
 }
