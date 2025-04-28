aa64c9eb8c75ee7331f56f5fcd172d63dd298eaf
Vinicius <viniciusgalmeida@gmail.com>
2025-04-28 14:38:22 -0300

    Add ContatoSection component with contact icons and improved layout

--- src/components/contato/ContatoSection.jsx
+++ src/components/contato/ContatoSection.jsx
@@ -1,13 +1,53 @@
 import "./contato.css";
+import linkedinLogo from "../../assets/linkedinLogo.png";
+import emailIcon from "../../assets/emailIcon.png";
+import whatsappLogo from "../../assets/whatsappLogo.png";
 
 function ContatoSection() {
   return (
-    <div className="containerContato">
+    <section className="containerContato">
       <h3 className="titleContato">Contato</h3>
-      <div className="">
-        <img />
+      <div className="contatoLinks">
+        <a
+          href="mailto:lucasvieirad2@gmai.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src={emailIcon}
+            className="iconStyle"
+            alt="Ícone do e-mail"
+            loading="lazy"
+          />
+        </a>
+        <a
+          href="https://wa.me/553189022905?text=Ol%C3%A1%2C%20Lucas%20Batista.%20Entrei%20em%20contato%20atrav%C3%A9s%20do%20seu%20site.
+"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src={whatsappLogo}
+            className="iconStyle"
+            alt="Ícone do WhatsApp"
+            loading="lazy"
+          />
+        </a>
+
+        <a
+          href="https://www.linkedin.com/in/lucas-batista-b2a350108/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src={linkedinLogo}
+            className="iconStyle"
+            alt="Ícone do LinkedIn"
+            loading="lazy"
+          />
+        </a>
       </div>
-    </div>
+    </section>
   );
 }
 
