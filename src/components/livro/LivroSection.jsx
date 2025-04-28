3c2b6fe64835fb44c3cb3de7a276dafbf6c32dda
Vinicius <viniciusgalmeida@gmail.com>
2025-04-28 08:26:34 -0300

    Implement LivroSection component with content and image for enhanced presentation

--- src/components/livro/LivroSection.jsx
+++ src/components/livro/LivroSection.jsx
@@ -1,5 +1,15 @@
 function LivroSection() {
-  return <div>LivroSection</div>;
+  return (
+    <div>
+      <img src={"src/assets/hospitalArchitectureBook.png"} />
+      <p>
+        Trabalho final do mestrado desenvolvido em Barcelona, sob orientação do
+        escritório de arquitetura Mario Corea, tese em forma de livro cujo tema
+        é Hospital de Emergência para Região Metropolitana de Belo Horizonte.
+      </p>
+      <button>Visualizar livro para donwload</button>
+    </div>
+  );
 }
 
 export default LivroSection;
